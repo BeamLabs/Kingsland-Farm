6bd28fec3c522c8efaf8a7379e22e17f725d3a9c
Mousecop <joaskew13@gmail.com>
2017-06-27 13:06:06 -0400

    adds real testimonials

--- src/pages/home/homeComponents/testimonialSection.js
+++ src/pages/home/homeComponents/testimonialSection.js
@@ -15,16 +15,16 @@ class TestimonialSection extends Component {
                 <Carousel className="testimonial-carousel" pauseOnHover={false} indicators={false} controls={false}>
                     <Carousel.Item>
                         <div className="testimonial-div">
-                            <p className="testimonial-quote">"I had an amazing exprience with my family at Kingsland Farm with my Family."</p>
-                            <p>- John Smith</p>
+                            <p className="testimonial-quote">"Loved our stay! She was able to commonindate us for a baby shower and it was beyond perfect! Plenty of room and such a lovely property! Also, the sweetest host ever! Thank you for allowing us to share your beautiful home!"</p>
+                            <p>- Kristy G.</p>
                             <StarRatingComponent starCount={5} name={'testimonial-stars'} value={5} editing={false}/>
                         </div>
                     </Carousel.Item>
                     <Carousel.Item>
                         <div className="testimonial-div">
-                            <p className="testimonial-quote">"The staff was very friendly and helpful with finding the right place to have a fun a vacation!"</p>
-                            <p>- Jamie Stone</p>
-                            <StarRatingComponent starCount={5} name={'testimonial-stars'} value={4} editing={false}/>
+                            <p className="testimonial-quote">"Absolutely gorgeous property and wonderful accommodations. Monica took care of every detail. Pristine and spotless. If you enjoy hearing wildlife at night and stargazing around the bonfire with modern conveniences just steps away, you will love this home."</p>
+                            <p>- Vicki B.</p>
+                            <StarRatingComponent starCount={5} name={'testimonial-stars'} value={5} editing={false}/>
                         </div>
                     </Carousel.Item>
                     <Carousel.Item>
@@ -37,14 +37,14 @@ class TestimonialSection extends Component {
                     <Carousel.Item>
                         <div className="testimonial-div">
                             <p className="testimonial-quote">"All the amenties that were offered made my families stay just that much better"</p>
-                            <p>- Johnny Bravo</p>
+                            <p>- Johnny Baine</p>
                             <StarRatingComponent starCount={5} name={'testimonial-stars'} value={4} editing={false}/>
                         </div>
                     </Carousel.Item>
                     <Carousel.Item>
                         <div className="testimonial-div">
                             <p className="testimonial-quote">"I would and have recommended this to all my friends if you are looking for a fun adventure!"</p>
-                            <p>- Seymour Butts</p>
+                            <p>- Justin Sanders</p>
                             <StarRatingComponent starCount={5} name={'testimonial-stars'} value={5} editing={false}/>
                         </div>
                     </Carousel.Item>
