195af07fd3e23c1930f1ab34db2873e2f04d13da
Ubuntu <ubuntu@ip-172-31-16-49.ec2.internal>
2017-06-28 16:06:32 +0000

    Fix deployment issues with path seperator

--- server/server.js
+++ server/server.js
@@ -2,7 +2,12 @@ const express = require('express')
 const app = express()
 const path = require('path');
 
-app.use('/', express.static(path.join(__dirname, '..' + path.delimiter + 'build')))
+console.log('dirName: ' + __dirname);
+app.use('/', express.static(path.join(__dirname, '..' + path.sep + 'build')))
+
+app.get('/hello', ((req, res) => {
+   res.send('hello world');
+}));
 
 app.listen(3000, function () {
   console.log('Kingsland Farm Website listening on port 3000!')
