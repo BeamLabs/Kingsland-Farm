2d488190515891510fe03ef5cd2fcad5a2838302
Mousecop <joaskew13@gmail.com>
2017-06-28 11:36:29 -0400

    Fixes merge conflict

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ class App extends Component {
             <Route exact path='/' component={Home}/>
             <Route path='/about' component={About}/>
             <Route path='/properties' component={Properties}/>
+            <Route exact path='/vacations' component={Vacations}/>
             <FooterComponent/>
           </div>
         </Router>
